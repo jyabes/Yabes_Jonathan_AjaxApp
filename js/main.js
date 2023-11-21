654da009d50072d4004121627c9a8f7e5c9beec6
JYabes <jonathanyabes@gmail.com>
2023-11-20 23:51:46 -0500

    Update the javascript
    Update the javascript

--- js/main.js
+++ js/main.js
@@ -1,4 +1,73 @@
 (() => {
+
+
+//variables
+const model = document.querySelector("#model");
+const hotspots = document.querySelectorAll(".Hotspot");
+
+const materialTemplate = document.querySelector("#material-template");
+const materialList = document.querySelector("#material-list");
+
+
+function getData() {
+  
+  fetch('https://swiftpixel.com/earbud/api/infoboxes')
+    .then(response => response.json())
+    .then(data => {
+      data.forEach((infoBox, index) => {
+        let selected = document.querySelector(`#hotspot-${index + 1}`);
+
+        const titleElement = document.createElement('h2');
+        titleElement.textContent = infoBox.heading;
+
+        const textElement = document.createElement('p');
+        textElement.textContent = infoBox.description;
+
+        selected.appendChild(titleElement);
+        selected.appendChild(textElement);
+      });
+    })
+    .catch(error => console.error('Error fetching infoboxes:', error));
+}
+
+getData();
+
+
+function loadMaterialInfo() {
+  fetch('https://swiftpixel.com/earbud/api/materials')
+    .then(response => response.json())
+    .then(data => {
+      data.forEach(material => {
+        // make a copy of the template
+        const clone = materialTemplate.content.cloneNode(true);
+
+        // fill the template
+        const materialHeading = clone.querySelector('.material-heading');
+        materialHeading.textContent = material.heading;
+
+        const materialDescription = clone.querySelector('.material-description');
+        materialDescription.textContent = material.description;
+
+        // append the populated template to the ul
+        materialList.appendChild(clone);
+      });
+    })
+    .catch(error => console.error('Error fetching materials:', error));
+}
+
+loadMaterialInfo();
+
+
+})();
+
+
+
+
+
+
+/**
+ * 
+ (() => {
   
 
   //variables
@@ -132,54 +201,10 @@
   });
 
 
-  const peopleCon = document.querySelector("#people-con");
-
-  let spinner = `<svg width="90" height="090" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><style>.spinner_EUy1{animation:spinner_grm3 1.2s infinite}.spinner_f6oS{animation-delay:.1s}.spinner_g3nX{animation-delay:.2s}.spinner_nvEs{animation-delay:.3s}.spinner_MaNM{animation-delay:.4s}.spinner_4nle{animation-delay:.5s}.spinner_ZETM{animation-delay:.6s}.spinner_HXuO{animation-delay:.7s}.spinner_YaQo{animation-delay:.8s}.spinner_GOx1{animation-delay:.9s}.spinner_4vv9{animation-delay:1s}.spinner_NTs9{animation-delay:1.1s}.spinner_auJJ{transform-origin:center;animation:spinner_T3O6 6s linear infinite}@keyframes spinner_grm3{0%,50%{animation-timing-function:cubic-bezier(.27,.42,.37,.99);r:1px}25%{animation-timing-function:cubic-bezier(.53,0,.61,.73);r:2px}}@keyframes spinner_T3O6{0%{transform:rotate(360deg)}100%{transform:rotate(0deg)}}</style><g class="spinner_auJJ"><circle class="spinner_EUy1" cx="12" cy="3" r="1"/><circle class="spinner_EUy1 spinner_f6oS" cx="16.50" cy="4.21" r="1"/><circle class="spinner_EUy1 spinner_NTs9" cx="7.50" cy="4.21" r="1"/><circle class="spinner_EUy1 spinner_g3nX" cx="19.79" cy="7.50" r="1"/><circle class="spinner_EUy1 spinner_4vv9" cx="4.21" cy="7.50" r="1"/><circle class="spinner_EUy1 spinner_nvEs" cx="21.00" cy="12.00" r="1"/><circle class="spinner_EUy1 spinner_GOx1" cx="3.00" cy="12.00" r="1"/><circle class="spinner_EUy1 spinner_MaNM" cx="19.79" cy="16.50" r="1"/><circle class="spinner_EUy1 spinner_YaQo" cx="4.21" cy="16.50" r="1"/><circle class="spinner_EUy1 spinner_4nle" cx="16.50" cy="19.79" r="1"/><circle class="spinner_EUy1 spinner_HXuO" cx="7.50" cy="19.79" r="1"/><circle class="spinner_EUy1 spinner_ZETM" cx="12" cy="21" r="1"/></g></svg>`;
-
-  function getData() {
-    peopleCon.innerHTML = spinner;
-    fetch("https://swiftpixel.com/earbud/api/infoboxes")
-    .then(response => response.json())
-    .then(people => {
-      console.log(people);
-
-      let ul = document.createElement("ul");
-      
-      people.results.forEach(result => {
-       
-        const li = document.createElement("li");
-
-        const img = document.createElement("img");
-        img.src = result.picture.thumbnail;
-
-        const h3 = document.createElement("h3");
-        h3.textContent = `${result.name.first} ${result.name.last}`;
-
-        const p = document.createElement("p");
-        p.textContent = result.email;
-
-        
-        li.appendChild(img);
-        li.appendChild(h3);
-        li.appendChild(p);
-        ul.appendChild(li);
-    });
-
-    peopleCon.innerHTML = "";
-    peopleCon.appendChild(ul);   
-
-    })
-    .catch(error => console.error(error)); //catch and report any errors
-  }
-
-  getData();
-
-
-
 
 })();
 
 
 
-
+**/
 
